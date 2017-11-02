27510c181d09b0f67e7ab0d5d05f9069a8e3ec20
AlexPurhalo <alexpurhalo@gmail.com>
2017-11-02 15:53:41 +0200

    Flex boxes usage

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class AlignItemsBasics extends Component {
 	render() {
 		return (
-			<View>
+			// Try setting `alignItems` to 'flex-start'
+			// Try setting `justifyContent` to `flex-end`.
+			// Try setting `flexDirection` to `row`.
+			<View style={{
+				flex: 1,
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
 				<View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-				<View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-				<View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+				<View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+				<View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
 			</View>
 		);
 	}
-}
+};
